b483cceaed236520c6de8f7bdb63d790386369c7
Anand <122715926+pigeio@users.noreply.github.com>
2025-03-26 23:54:39 +0530

    Time: 58 ms (35.92%), Space: 18.6 MB (91.54%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+       int n = matrix.size(), m = matrix[0].size();
+        int row = 0, col = m - 1;
+
+        // Start from the top-right corner
+        while (row < n && col >= 0) {
+            if (matrix[row][col] == target)
+                return true;
+            else if (matrix[row][col] > target)
+                col--;  // Move left
+            else
+                row++;  // Move down
+        }
+        return false;
+    }
+};
