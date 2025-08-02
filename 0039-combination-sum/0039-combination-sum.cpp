8c380f90bff28ac87d0389dc57d921f01720048a
Anand <122715926+pigeio@users.noreply.github.com>
2025-08-02 17:58:01 +0530

    Time: 0 ms (100%), Space: 14.4 MB (39.67%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>>result;
+
+    void rocket(int i ,vector<int>& candidates , int tar, int s ,vector<int>&ds ,int N){
+        if(i >= N){
+            if(s == tar){
+                result.push_back(ds);
+            }
+            return;
+        }
+        
+        if(s+candidates[i] <= tar){
+        ds.push_back(candidates[i]);
+        rocket(i ,candidates ,tar ,s + candidates[i], ds,  N);
+        ds.pop_back();
+        }
+        rocket(i+1 ,candidates , tar,s , ds ,N);
+
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int N = candidates.size();
+        vector<int>ds;
+        rocket(0, candidates, target ,0 ,ds ,N);
+        return result;
+    }
+};
